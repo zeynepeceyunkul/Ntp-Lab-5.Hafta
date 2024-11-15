f6d35edde4438e12ef117cac34970bb8cef59221
zeynepeceyunkul <eceyunkul44@gmail.com>
2024-11-15 23:47:56 +0300

    Kütüphane ve Kitap

---
+++ Kutuphane2/Kutuphane2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kutuphane2
+{
+    class Kitap
+    {
+        public string Baslik { get; set; }
+        public string Yazar { get; set; }
+
+        public Kitap(string b, string y)
+        {
+            Baslik = b;
+            Yazar = y;
+        }
+    }
+
+    class Kutuphane
+    {
+        public string Ad { get; set; }
+        public List<Kitap> Kitaplar = new List<Kitap>();
+
+        public Kutuphane(string a)
+        {
+            Ad = a;
+        }
+
+        public void KitapEkle(Kitap kitap)
+        {
+            this.Kitaplar.Add(kitap);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
