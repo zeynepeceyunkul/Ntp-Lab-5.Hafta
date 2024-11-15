857a899facab3d94d7157bf22045ac964ba5bf09
zeynepeceyunkul <eceyunkul44@gmail.com>
2024-11-15 23:39:50 +0300

    Ürün ve Sipariş

---
+++ UrunveSip/UrunveSip/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UrunveSip
+{
+    class Siparis
+    {
+        public DateTime Tarih { get; set; }
+        public decimal Toplam { get; set; }
+
+        Siparis(DateTime t, decimal top)
+        {
+            Tarih = t;
+            Toplam = top;
+        }
+    }
+
+    class Urun
+    {
+        public string Ad { get; set; }
+        private int Fiyat { get; set; }
+
+        public Urun(string a, int f)
+        {
+            Ad = a;
+            Fiyat = f;
+        }
+
+        public void UrunBilgisi()
+        {
+            Console.WriteLine(Ad + " " + Fiyat);
+            
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
