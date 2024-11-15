957e3f4919a6aad46f66c4d4f92f0a9339a8392c
zeynepeceyunkul <eceyunkul44@gmail.com>
2024-11-15 23:50:11 +0300

    Okul ve Öğrenci

---
+++ Okulv/Okulv/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Okulv
+{
+    class Ogrenci
+    {
+        public string Ad { get; set; }
+        public int Yas { get; set; }
+
+        public Ogrenci(string A, int y)
+        {
+            Ad = A;
+            Yas = y;
+        }
+        public void OgrenciBilgisi()
+        {
+            Console.WriteLine(Ad + " " + Yas);
+        }
+        
+    }
+
+    class Okul
+    {
+        public string Ad { get; set; }
+        public List<Ogrenci> Ogrenciler = new List<Ogrenci>();
+
+        public Okul(string a)
+        {
+            Ad = a;
+            
+        }
+        public void OgrenciOlustur(Ogrenci ogrenci)
+        {
+            this.Ogrenciler.Add(ogrenci);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
