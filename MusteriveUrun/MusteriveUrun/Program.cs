368513444afd2cd4f6ccacb96f7a50fccbdb45bc
zeynepeceyunkul <eceyunkul44@gmail.com>
2024-11-15 23:40:29 +0300

    Müşteri ve Sipariş

---
+++ MusteriveUrun/MusteriveUrun/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusteriveUrun
+{
+    class Siparis
+    {
+        public DateTime Tarih { get; set; }
+        public string Durum { get; set; }
+
+        Siparis(DateTime t, string d)
+        {
+            Tarih = t;
+            Durum = d;
+        }
+    }
+
+    class Urun
+    {
+        public string Ad { get; set; }
+        public string Telefon { get; set; }
+
+        public Urun(string a, string t)
+        {
+            Ad = a;
+            Telefon = t;
+        }
+
+        public void SiparisVer()
+        {
+           string Siparis = Ad + Telefon;
+            
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
