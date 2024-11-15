9854bb9db038bc21f57e3cc3cf9bd1936a47be6d
zeynepeceyunkul <eceyunkul44@gmail.com>
2024-11-15 23:44:19 +0300

    Şirket ve Çalışan

---
+++ Sirket/Sirket/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sirket
+{
+    class Calisan
+    {
+        public string Isim { get; set; }
+        public int Maas { get; set; }
+        public Sirket Sirket { get; set; }
+
+        public Calisan(string i, int m)
+        {
+            Isim = i;
+            Maas = m;
+        }
+
+        public void SirketAtama(Sirket sirket)
+        {
+            Sirket = sirket;
+        }
+    }
+    class Sirket
+    {
+        public string Ad { get; set; }
+        public string Konum { get; set; }
+        public List<Calisan> Calisanlar = new List<Calisan>();
+
+        public void CalisanEkle(Calisan calisan)
+        {
+            this.Calisanlar.Add(calisan);
+            Console.WriteLine(calisan.Isim + " çalışan eklenmiştir.");
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
