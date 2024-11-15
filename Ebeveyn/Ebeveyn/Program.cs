cb319c6e6465c8869bd44dbdf1436e594567f778
zeynepeceyunkul <eceyunkul44@gmail.com>
2024-11-15 23:45:27 +0300

    Ebeveyn ve Çocuk

---
+++ Ebeveyn/Ebeveyn/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ebeveyn
+{
+    class Cocuk
+    {
+        public string Ad { get; set; }
+        public int Yas { get; set; }
+        public Ebeveyn Ebeveyn { get; set; }
+
+        public Cocuk(string a, int y)
+        {
+            Ad = a;
+            Yas = y;
+        }
+        public void EbeveynAtama(Ebeveyn ebeveyn)
+        {
+            Ebeveyn = ebeveyn;
+        }
+        
+
+    }
+    class Ebeveyn
+    {
+        public string Ad { get; set; }
+        public int Yas { get; set; }
+        public List<Cocuk> Cocuklar = new List<Cocuk>();
+
+        public Ebeveyn(string a, int y)
+        {
+            Ad = a;
+            Yas = y;
+        }
+
+        public void CocukEkle(Cocuk cocuk)
+        {
+            this.Cocuklar.Add(cocuk);
+            Console.WriteLine(cocuk.Ad + " çocuk eklenmiştir.");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
